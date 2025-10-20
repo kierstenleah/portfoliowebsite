5745cfd9fe6a119dd2fc45a62061fdae1ba1e41e
Kiersten Aubin <kiersten.aubin1@gmail.com>
2025-10-20 15:09:51 -0400

    added contact page and made nav buttons work

--- script.js
+++ script.js
@@ -9,18 +9,27 @@ function openGames(){
     gameFolders.style.display='flex';
     webFolders.style.display='none';
     moreFolders.style.display='none';
+    gameButton.style.backgroundColor = 'pink';
+    webButton.style.backgroundColor = 'transparent';
+    moreButton.style.backgroundColor = 'transparent';
 };
 
 function openWeb(){
     webFolders.style.display='flex';
     gameFolders.style.display='none';
     moreFolders.style.display='none';
+    webButton.style.backgroundColor = 'pink';
+    gameButton.style.backgroundColor = 'transparent';
+    moreButton.style.backgroundColor = 'transparent';
 };
 
 function openMore(){
     moreFolders.style.display='flex';
     webFolders.style.display='none';
     gameFolders.style.display='none';
+    moreButton.style.backgroundColor = 'pink';
+    gameButton.style.backgroundColor = 'transparent';
+    webButton.style.backgroundColor = 'transparent';
 };
 
 gameButton.onclick = openGames;
